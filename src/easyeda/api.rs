d6e41ef19979ca9c49703e65b10315c4ce591cc6
linkyourbin <ybinlew@gmail.com>
2026-02-27 23:49:15 +0800

    Bump version to 1.0.13

--- src/easyeda/api.rs
+++ src/easyeda/api.rs
@@ -10,7 +10,7 @@ impl EasyedaApi {
     pub fn new() -> Self {
         Self {
             client: Client::builder()
-                .user_agent("nlbn/1.0.12")
+                .user_agent("nlbn/1.0.13")
                 .build()
                 .expect("Failed to create HTTP client"),
         }
